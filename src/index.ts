7840c97af1050a2e4acfc5afa9a73547a62b9fcd
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2020-10-12 16:08:05 -0400

    Added support for secondless inputs

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-export function toInputDatetimeLocal (date: Date): string {
-	return date.toLocaleString("sv-SE", {
+export function toInputDatetimeLocal (date: Date, includeSeconds = true): string {
+	const options: Intl.DateTimeFormatOptions = {
 		year: "numeric",
 		month: "2-digit",
 		day: "2-digit",
 		hour: "2-digit",
-		minute: "2-digit",
-		second: "2-digit"
-	}).replace(" ", "T");
+		minute: "2-digit"
+	};
+	if(includeSeconds) options.second = "2-digit";
+	return date.toLocaleString("sv-SE", options).replace(" ", "T");
 }
 export function fromInputDatetimeLocal (s: string): Date {
 	return new Date(s);
 }
 
-export function toInputTime(date: Date): string {
-	return date.toLocaleString("sv-SE", {
+export function toInputTime(date: Date, includeSeconds = true): string {
+	const options: Intl.DateTimeFormatOptions = {
 		hour: "2-digit",
-		minute: "2-digit",
-		second: "2-digit"
-	});
+		minute: "2-digit"
+	};
+	if(includeSeconds) options.second = "2-digit";
+	return date.toLocaleString("sv-SE", options);
 }
 export function fromInputTime(s: string, day = new Date): Date {
 	const dayString = day.toLocaleString("sv-SE", {
